fff2b7045e6bf8ccd0509e2a69313ea3f2b29530
RonnyKn <ronnykurniawn@gmail.com>
2022-12-31 14:11:52 +0700

    adding footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import About from "./components/about/About"
 import Alur from "./components/alur/Alur"
 import Benefit from "./components/benefit/Benefit"
+import Footer from "./components/footer/Footer"
 import Form from "./components/form/Form"
 import Header from "./components/header/Header"
 import TopBar from "./components/topBar/TopBar"
@@ -15,6 +16,7 @@ function App() {
       <About />
       <Form />
       <Alur />
+      <Footer />
     </>
   )
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css"
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer className="container footer-container">
+      <h4>Copyright (c) 2017 Pilihkartucom and AXA All Rights Reserved</h4>
+    </footer>
+  )
+}
+
+export default Footer
